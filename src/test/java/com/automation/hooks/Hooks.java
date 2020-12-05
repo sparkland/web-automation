d40bb892e18539e273f6ffb121b03fb2d24701f1
Mark Robertson <mark.craig.robertson.1986@gmail.com>
2020-12-05 20:56:03 +0000

    Added scenario failure screenshot after step action

--- src/test/java/com/automation/hooks/Hooks.java
+++ src/test/java/com/automation/hooks/Hooks.java
@@ -2,7 +2,11 @@ package com.automation.hooks;
 
 import com.automation.base.Base;
 import io.cucumber.java.After;
+import io.cucumber.java.AfterStep;
 import io.cucumber.java.Before;
+import io.cucumber.java.Scenario;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 
 import java.net.MalformedURLException;
 
@@ -19,6 +23,13 @@ public class Hooks extends Base {
     public void setup() throws MalformedURLException {
         String driverSelection = System.getProperty("driver") == null ? "chrome" : System.getProperty("driver");
         base.getDriver(driverSelection);
+    }
+
+    @AfterStep
+    public void stepTeardown(Scenario scenario) {
+        if (scenario.isFailed()) {
+            scenario.attach(((TakesScreenshot) driver).getScreenshotAs(OutputType.BYTES), "image/png", "Scenario Failure Screenshot");
+        }
     }
 
     @After
